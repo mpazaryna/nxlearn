6c7018bf494d24f5f0f84dd2e3a3a22faffdbdc3
mpazaryna <mpazbot@gmail.com>
2022-07-14 10:17:34 -0400

    feat: add feature-library ui for store
    Add a specific feature library into lib for the display of the game detail item.

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -8,6 +8,10 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 
+import { Route, Routes, Link } from "react-router-dom";
+
+import { StoreFeatureLibrary } from "@nxlearn/store/feature-library";
+
 export function App() {
   return (
     <>
@@ -47,6 +51,47 @@ export function App() {
           ))}
         </div>
       </div>
+
+      {/* START: routes */}
+      {/* These routes and navigation have been generated for you */}
+      {/* Feel free to move and update them to fit your needs */}
+      <br />
+      <hr />
+      <br />
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/feature-library">StoreFeatureLibrary</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route.{" "}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route path="/feature-library" element={<StoreFeatureLibrary />} />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
     </>
   );
 }
